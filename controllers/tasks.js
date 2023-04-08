eb17d4b919b7679a0f092f632236e1f1e513f49c
ali mert inal <mert.inal@gmail.com>
2023-04-08 18:15:49 +0200

    add first controller file tasks

---
+++ controllers/tasks.js
@@ -0,0 +1,5 @@
+const getAllTasks = async (req, res) => {
+    res.send('all items are here(controller task.js)')
+}
+
+module.exports = { getAllTasks }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router =  express.Router()
 
+const {getAllTasks} = require('../controllers/tasks')
 
-router.route('/').get((req, res) => {
-    res.send('all items are here')
-})
+router.route('/').get(getAllTasks)
 
 module.exports = router
