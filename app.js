8c3e0694d5301e8b8b1c5aeabefc983572fe518d
ali mert inal <mert.inal@gmail.com>
2023-04-08 23:01:43 +0200

    add async connection to DB

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect.js')
 
 //middleware
+
 app.use(express.json())
 
 
@@ -14,8 +16,21 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasks)
 
 const port = 3000
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-}   )
+
+
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, () => {console.log('Server is up on port ' + port)})
+        } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+        
+
+
+
 
 app.use('api/v1/tasks',tasks) 
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,14 @@
-const connectionString = 'mongodb+srv://atsinegifilozof:mertovic@nodejsexpresfrsitof4pro.hwc0bhc.mongodb.net/MERTSEXPRESSJOURNEY?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+const connectionString = 'mongodb+srv://atsinegifilozof:mertovic@nodejsexpresfrsitof4pro.hwc0bhc.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
+
+const connectDB = (url) => {
+       return mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true })
+     
+    }
+
+
+module.exports = connectDB
