1c0c06b60d7bae72910b4212dc4692b4fd7b37cf
ali mert inal <mert.inal@gmail.com>
2023-04-08 20:20:57 +0200

    add routes update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,17 @@ const getAllTasks = async (req, res) => {
     res.send('all items are here(controller task.js)')
 }
 
-module.exports = { getAllTasks }
+const createTask = async (req, res) => {
+    res.json(req.body)
+}
+const getTask = async (req, res) => {
+    res.json({id:req.params.id})
+}
+const updateTask = async (req, res) => {
+    res.send('update task')
+}
+const deleteTask = async (req, res) => {
+    res.send('delete task')
+}
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
