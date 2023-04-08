1dd15d9f491db4a2e236c71a9f05368629c594a8
obifemi <54670899+obifemi@users.noreply.github.com>
2023-04-08 23:03:49 +0200

    add hide connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://atsinegifilozof:mertovic@nodejsexpresfrsitof4pro.hwc0bhc.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://'
 
 const connectDB = (url) => {
        return mongoose.connect(connectionString, {
