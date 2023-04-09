d6e5cf54bd6e8c4205a0de93b760983269f6ba09
ali mert inal <mert.inal@gmail.com>
2023-04-09 02:03:39 +0200

    add task validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,18 @@
+const Task = require('../models/Task')
+
 const getAllTasks = async (req, res) => {
     res.send('all items are here(controller task.js)')
 }
 
 const createTask = async (req, res) => {
-    res.json(req.body)
+try{ 
+
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
+catch(err){
+    res.status(400).json({msg:err})
+}}
 const getTask = async (req, res) => {
     res.json({id:req.params.id})
 }
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,4 @@
 const mongoose = require('mongoose')
-<<<<<<< HEAD
-const connectionString = ''
-=======
-const connectionString = 'mongodb+srv://'
->>>>>>> 1dd15d9f491db4a2e236c71a9f05368629c594a8
-
 const connectDB = (url) => {
        return mongoose.connect(url, {
             useNewUrlParser: true,
